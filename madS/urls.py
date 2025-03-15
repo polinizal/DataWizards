599ef417d776ddc7535c01c192b6dc68d8abfd6a
polinizal <polina.staneva@yahoo.com>
2025-03-15 15:29:51 +0200

    add html pages and requirements

--- madS/urls.py
+++ madS/urls.py
@@ -1,7 +1,10 @@
-from django.urls import path
-from .views import home
-
-
+from django.urls import path, include
+from.import views
 urlpatterns = [
-    path('', home, name='home'),  # Home page
+  path('', views.home, name='home'),
+  path('blogs/', views.blogs, name='blogs'),
+  path('blog-list/', views.bloglist, name='blog-list'),
+  path('blog-details/', views.blog_details, name='blog-list'),
+  path('create-blog/', views.create_blog, name='create-blog'),
+  path('edit-blog/', views.edit_blog, name='edit-blog'),
 ]
--- madS/views.py
+++ madS/views.py
@@ -1,4 +1,20 @@
 from django.shortcuts import render
 
-def home(request):  
-    return render(request, 'crm/index.html')  
+# Create your views here.
+def home(request):
+    return render(request, 'crm/index.html')
+
+def blogs(request):
+    return render(request, 'crm/base.html')
+
+def bloglist(request):
+    return render(request, 'crm/blog_list.html')
+
+def blog_details(request):
+    return render(request, 'crm/blog_details.html')
+
+def create_blog(request):
+    return render(request, 'crm/create_blog.html')
+
+def edit_blog(request):
+    return render(request, 'crm/edit_blog.html')
