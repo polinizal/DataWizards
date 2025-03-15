c8aaba8609364f3a5ef34ec5143e29cb644eba25
boza20000 <boristsvetkov912@gmail.com>
2025-03-15 13:07:44 +0200

    fix home page error

--- madS/views.py
+++ madS/views.py
@@ -1,7 +1,4 @@
+from django.shortcuts import render
 
-from django.shortcuts import render, get_object_or_404, redirect
-
-
-def home(request):
+def home(request):  
     return render(request, 'crm/index.html')  
-
