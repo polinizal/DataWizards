c004dbd63cdfeeebfe634c0463ab895a5a823c76
boza20000 <boristsvetkov912@gmail.com>
2025-03-15 12:21:11 +0200

    bug fix

--- madS/views.py
+++ madS/views.py
@@ -1,16 +1,7 @@
-<<<<<<< HEAD
+
 from django.shortcuts import render, get_object_or_404, redirect
 
 
 def home(request):
     return render(request, 'crm/index.html')  
-
-=======
-# from django.shortcuts import render, get_object_or_404, redirect
-# from .models import Blog
-
-
-# def home(request):
-#    return render(request, 'crm/index.html')  # Updated path
->>>>>>> 3077bac17772c728f899608437f419aeee71f27e
 
