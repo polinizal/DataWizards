ca69e0550fc6a883993068006883cc4f41878765
polinizal <polina.staneva@yahoo.com>
2025-03-16 04:33:25 +0200

    add register logic

--- members/urls.py
+++ members/urls.py
@@ -3,4 +3,5 @@ from . import views
 urlpatterns = [
     path('login-user', views.login_user, name='login'),
     path('logout-user', views.logout_user, name='logout'),
+    path('register-user', views.register_user, name='register'),
 ]
--- members/views.py
+++ members/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render, redirect
-from django.contrib.auth import login, authenticate, logout
+from django.contrib.auth import authenticate, login, logout
 from django.contrib import messages
+from django.contrib.auth.forms import UserCreationForm
+from .forms import RegisterUserForm
 
 # Create your views here.
 def login_user(request):
@@ -23,3 +25,21 @@ def logout_user(request):
 	logout(request)
 	messages.success(request, ("You Were Logged Out!"))
 	return redirect('blog-list')
+
+def register_user(request):
+	if request.method == "POST":
+		form = RegisterUserForm(request.POST)
+		if form.is_valid():
+			form.save()
+			username = form.cleaned_data['username']
+			password = form.cleaned_data['password1']
+			user = authenticate(username=username, password=password)
+			login(request, user)
+			messages.success(request, ("Registration Successful!"))
+			return redirect('blog-list')
+	else:
+		form = RegisterUserForm()
+
+	return render(request, 'authenticate/register_user.html', {
+		'form':form,
+		})
